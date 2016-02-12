3a3d52aca13341ad0bf976c67ca24000e1c9ede1
derek mcquay <derekmcquay@gmail.com>
2016-02-12 09:35:30 -0800

    added rail fence and split transposition ciphers

--- psyfer/transposition.go
+++ psyfer/transposition.go
@@ -5,7 +5,7 @@ import (
 	"math/rand"
 )
 
-func Transpose(input string) string {
+func TransposeRandom(input string) string {
 	shuffle := ""
 	list := rand.Perm(len(input))
 	fmt.Println(list)
@@ -13,4 +13,33 @@ func Transpose(input string) string {
 		shuffle += string(input[i])
 	}
 	return shuffle
+}
+
+func TransposeRailFence(input string) string {
+	rf := ""
+	for i := 0; i < len(input); i += 2 {
+		rf += string(input[i])
+	}
+	for i := 1; i < len(input); i += 2 {
+		rf += string(input[i])
+	}
+	return rf
+}
+
+func TransposeSplit(input string) string {
+	split := ""
+	length := len(input)
+	first := input[:length/2]
+	second := input[length/2:]
+	if length%2 == 0 {
+		for i, _ := range first {
+			split += string(first[i]) + string(second[i])
+		}
+	} else {
+		for i, _ := range first {
+			split += string(first[i]) + string(second[i])
+		}
+		split += string(second[len(second)-1])
+	}
+	return split
 }
